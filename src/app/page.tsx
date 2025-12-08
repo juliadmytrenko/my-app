90f18e2bbd2955b1cfb4caa4681f535d369a2017
Julia Dmytrenko <dmytrenkojulia@gmail.com>
2025-12-08 18:57:08 +0100

    remove pl of a list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         <h2 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-purple-600">
           Latest Posts
         </h2>
-        <ul className="space-y-4 list-none">
+        <ul className="space-y-4 list-none pl-0">
           {posts.map((post) => (
             <li
               key={post.id}
